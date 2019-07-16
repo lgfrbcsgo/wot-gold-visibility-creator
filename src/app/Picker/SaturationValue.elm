8f564c6a8228e8684818cbeb81f52629aa2bdace
lgfrbcsgo <33672090+lgfrbcsgo@users.noreply.github.com>
2019-07-16 19:55:56 +0200

    Implement color conversion only once

--- src/app/Picker/SaturationValue.elm
+++ src/app/Picker/SaturationValue.elm
@@ -5,6 +5,7 @@ import Color exposing (..)
 import Html exposing (Html, div)
 import Html.Attributes exposing (height, style, width)
 import Math.Vector2 exposing (Vec2, vec2)
+import Math.Vector3 exposing (Vec3, vec3)
 import Picker.Shared exposing (matrixInput)
 import Slider
 import WebGL exposing (Mesh, Shader)
@@ -95,13 +96,16 @@ view color (Model model) =
             div [ style "backgroundColor" thumbBackgroundColor ]
                 []
 
+        baseColor =
+            color |> toRgba
+
         viewBackground =
             WebGL.toHtml [ width 100, height 100 ]
                 [ WebGL.entity
                     vertexShader
                     fragmentShader
                     mesh
-                    { hue = color |> toHsva |> .hue }
+                    { baseColor = vec3 baseColor.red baseColor.green baseColor.blue }
                 ]
     in
     matrixInput Slider viewThumb viewBackground relativePosition model
@@ -129,36 +133,34 @@ mesh =
 ---- SHADERS ----
 
 
-vertexShader : Shader { position : Vec2 } { hue : Float } { saturationValue : Vec2 }
+vertexShader : Shader { position : Vec2 } { baseColor : Vec3 } { saturation : Float, value : Float }
 vertexShader =
     [glsl|
         attribute vec2 position;
-        varying vec2 saturationValue;
+        varying float saturation;
+        varying float value;
 
         void main () {
-            saturationValue = (position + 1.0) / 2.0;
+            vec2 normalizedPosition = (position + 1.0) / 2.0;
+            saturation = normalizedPosition.x;
+            value = normalizedPosition.y;
             gl_Position = vec4(position, 0.0, 1.0);
         }
     |]
 
 
-fragmentShader : Shader {} { hue : Float } { saturationValue : Vec2 }
+fragmentShader : Shader {} { baseColor : Vec3 } { saturation : Float, value : Float }
 fragmentShader =
     [glsl|
         precision mediump float;
 
-        uniform float hue;
-        varying vec2 saturationValue;
-
-        // Source: http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
-        vec3 hsvToRgb(vec3 c) {
-            vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-            vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-            return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
-        }
+        uniform vec3 baseColor;
+        varying float saturation;
+        varying float value;
 
         void main () {
-            vec3 hsv = vec3(hue, saturationValue);
-            gl_FragColor = vec4(hsvToRgb(hsv), 1.0);
+            vec3 whiteOverlayed = baseColor * saturation + 1.0 - saturation;
+            vec3 blackOverlayed = whiteOverlayed * value;
+            gl_FragColor = vec4(blackOverlayed, 1.0);
         }
     |]
